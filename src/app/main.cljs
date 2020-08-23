5f81f184023e1dfff94e7a9472c46f425a791aba
nanoni17728 <nanoni@riseup.net>
2020-08-23 21:17:46 +0200

    Draw a parrot picture on a canvas and add a slider below

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -6,11 +6,25 @@
 (ns app.main
   (:require [rum.core :as rum]))
 
-(rum/defc label
-  [text]
-  [:div {:class "label"} text])
+(enable-console-print!)
+
+(def red-percent (atom 100))
+(def canvas (.getElementById js/document "picture"))
+(def ctx (.getContext canvas "2d"))
+(def img (js/Image.))
+(.addEventListener img "load"
+                   (fn []
+                     (let [width  (/ (.-width img) 2)
+                           height (/ (.-height img) 2)]
+                       (set! (.-width canvas) width)
+                       (set! (.-height canvas) height)
+                     (.drawImage ctx img 0 0 width height))))
+(set! (.-src img) "parrot.jpg")
+
+(rum/defc slider []
+  [:input {:type "range" :min 0 :max 100 :defaultValue "100" :onChange (fn [e] (reset! red-percent (.. e -target -value)))}])
 
 (defn main!
   []
-  (rum/mount (label "test") (.getElementById js/document "hook"))
+  (rum/mount (slider) (.getElementById js/document "hook"))
   (js/console.log "Hello World! This is a test"))
