bd40e555a0a77e3b0b13889ebc525071d32ec6cd
nanoni17728 <nanoni@riseup.net>
2020-08-24 18:30:34 +0200

    WIP trying to figure out how to make fast transformation on the data

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -9,10 +9,17 @@
 
 (enable-console-print!)
 
+
 (def amount (atom 1.0))
 (def canvas (.getElementById js/document "picture"))
 (def ctx (.getContext canvas "2d"))
 (def img (js/Image.))
+
+(defn multiply [data]
+  (map (partial unchecked-multiply @amount) data))
+
+(defn callback [value index array]
+  (aset array index (* @amount value)))
 
 (defn after-load
   []
@@ -20,18 +27,18 @@
         height (/ (.-height img) 2)]
     (set! (.-width canvas) width)
     (set! (.-height canvas) height)
-    (def original (.getImageData ctx 0 0 width height))
     (.drawImage ctx img 0 0 width height)
-    (.putImageData ctx 
-                  (js/ImageData. (u8/fill (.-data original) (* 255 @amount)) (.-width original)) 0 0)))
+    (def original (.getImageData ctx 0 0 width height))
+    (def clj-data (vec (js->clj (.-data original))))))
+
 (.addEventListener img "load" after-load)
 (set! (.-src img) "parrot.jpg")
 
 (defn test-watcher
   [key watched old-state new-state]
   (.putImageData ctx
-                 (js/ImageData. (u8/fill (.-data original) (* 100 new-state)) (.-width original)) 0 0)
-  #(js/console.log "key:" (str key) "watched:" (str watched) "old-state:" (str old-state) "new-state:" (str new-state)))
+                 (js/ImageData. (.from js/Uint8ClampedArray (clj->js (multiply clj-data))) (.-width original)) 0 0)
+  #_(js/console.log "key:" (str key) "watched:" (str watched) "old-state:" (str old-state) "new-state:" (str new-state)))
 (add-watch amount :watcher test-watcher)
 
 (rum/defc slider []
